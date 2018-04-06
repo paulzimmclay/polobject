ab591ad9984cc94a7cd9613a6ce469b7c2cf9a0d
Paul Zimmerman-Clayton <paul.zimmerman.clayton@gmail.com>
2018-04-06 13:30:57 -0500

    first commit

---
+++ polobject.js
@@ -0,0 +1,160 @@
+const elizabethSangerProfile = Object.create({
+  name: {
+    value: "Elizabeth Sanger",
+    writable: true,
+    enumerable: true
+  },
+  district: {
+    value: "District 5",
+    writable: true,
+    enumerable: true
+  },
+  biography: {
+    value: "Bio goes here",
+    writable: true,
+    enumerable: true
+  },
+  image_gallery: {
+    headshot: { 
+      value: "http://www.elizabethsanger.com/headshot.jpg",
+      writable: true,
+      enumerable: true 
+    },
+    familyPicture: { 
+      value: "http://www.elizabethsanger.com/familypicture.jpg",
+      writable: true,
+      enumerable: true
+    },
+    consituentPicture: {
+      value: "http://www.elizabethsanger.com/headshot.jpg",
+      writable: true,
+      enumerable: true
+    }
+  }
+})
+
+const elizabethSangerPlatform = Object.create({
+  missionStatement: {
+    value: "Mission statement goes here.",
+    writable: true,
+    enumerable: true
+  },
+  platformStatements: {
+    taxes: {
+      value: "no taxes at all, plus refunds for taxes already paid",
+      writable: true,
+      enumerable: true
+    },
+    jobs: {
+      value: "no jobs at all, plus free money",
+      writable: true,
+      enumerable: true
+    },
+    infrastructure: {
+      value: "for the elimination of all infrastructure",
+      writable: true,
+      enumerable: true
+    },
+    healthCare: {
+      value: "insurance will be abolished",
+      writable: true,
+      enumerable: true
+    },
+    crime: {
+      value: "elizabeth is pro-crime",
+      writable: true,
+      enumerable: true
+    }
+  }
+})
+
+const elizabethSangerVolunteers = Object.create({
+  profiles: [
+    {
+      name: {
+        value: "Paul Zimmerman-Clayton",
+        writable: true,
+        enumerable: true
+      },
+      age: {
+        value: "67",
+        writable: true,
+        enumerable: true
+      },
+      opinions: {
+        value: "all of them",
+        writable: true,
+        enumerable: true
+      }
+    },
+    {
+      name: {
+        value: "Katie Zimmerman-Clayton",
+        writable: true,
+        enumerable: true
+      },
+      age: {
+        value: "68",
+        writable: true,
+        enumerable: true
+      },
+      opinions: {
+      value: "most of them",
+      writable: true,
+      enumerable: true
+      }
+    },
+    {
+      name: {
+        value: "Penny Zimmerman-Clayton",
+        writable: true,
+        enumerable: true
+      },
+      age: {
+        value: "0",
+        writable: true,
+        enumerable: true
+      },
+      opinions: {
+        value: "yes to cheerios",
+        writable: true,
+        enumerable: true
+      }
+    },
+    {
+      name: {
+        value: "James Zimmerman-Clayton",
+        writable: true,
+        enumerable: true
+      },
+      age: { 
+        value: "3 going on 16",
+        writable: true,
+        enumerable: true
+      },
+      opinions: {
+        value: "strong regarding trucks, food, naps",
+        writable: true,
+        enumerable: true
+      }
+    }
+  ]
+})
+
+const elizabethSangerInfo = Object.create({
+  calendar: {
+    value: "URL to calendar",
+    writable: true,
+    enumerable: true
+  },
+  voterRegistration: {
+    value: "http://www.elizabethsanders.com/vote-here",
+    writable: true,
+    enumerable: true
+  },
+  donationForm: {
+    value: "http://www.elizabethsanders.com/gimme-money",
+    writable: true,
+    enumerable: true
+  }
+})
